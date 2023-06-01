ea4f975b7cc8bc8337331ab77434984d1afc9d3b
jkbh <jakob_h@outlook.de>
2023-06-01 21:09:22 +0200

    Add JsonLines Export of Shoe Items

--- shoemonitor/pipelines.py
+++ shoemonitor/pipelines.py
@@ -6,8 +6,25 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+from scrapy.exporters import JsonLinesItemExporter
+import os
 
 class ShoemonitorPipeline:
+    def __init__(self) -> None:
+        self.exporter = None
+
+    def open_spider(self, spider):
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        project_dir = os.path.split(current_dir)[0]
+        items_path = os.path.join(project_dir, 'web', 'items.json')
+
+        self.file = open(items_path, 'wb')
+        self.exporter = JsonLinesItemExporter(self.file)
+
+    def close_spider(self, spider):
+        self.file.close()
+        
     def process_item(self, item, spider):
+        self.exporter.export_item(item)
         return item
+    
--- shoemonitor/settings.py
+++ shoemonitor/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = "shoemonitor.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = "shoemonitor (+http://www.yourdomain.com)"
+USER_AGENT = "shoemonitor (+http://github.com/jkbh/shoemonitor)"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -62,9 +62,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "shoemonitor.pipelines.ShoemonitorPipeline": 300,
-#}
+ITEM_PIPELINES = {
+   "shoemonitor.pipelines.ShoemonitorPipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
